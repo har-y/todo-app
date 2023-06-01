240f95a2a998bb483f56ed003ea8c4a2c0bf7148
har-y <jakub.harenda@gmail.com>
2023-06-01 15:33:34 +0200

    add favicon change document title

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>clean project</h1>;
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h1>todo app</h1>
+    </div>
+  );
 }
 
 export default App;
