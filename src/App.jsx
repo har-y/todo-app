51d4a60175681e59ffa651823b547865f96f4c3f
har-y <jakub.harenda@gmail.com>
2023-06-03 00:54:55 +0200

    add prototype 'display' todo style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import ItemTodo from './components/itemtodo/ItemTodo';
 import NewTodo from './components/newtodo/NewTodo';
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-400">
-      <div className="my mx-auto flex max-w-md flex-col items-center space-y-5 py-20 md:max-w-xl">
-        <h1 className="text-2xl font-bold">TODO LIST</h1>
+      <div className="my mx-auto flex max-w-md flex-col space-y-5 py-20 md:max-w-xl">
+        <h1 className="mx-auto text-2xl font-bold">TODO LIST</h1>
         <NewTodo />
+        <ItemTodo />
       </div>
     </div>
   );
---
+++ src/components/itemtodo/ItemTodo.jsx
@@ -0,0 +1,21 @@
+const ItemTodo = () => {
+  return (
+    <ul className="w-full pt-5">
+      <li className="flex w-full justify-between">
+        <div className="ml-2 mr-5 w-full border-b-2 border-neutral-100 border-opacity-100 py-2 dark:border-opacity-50">
+          <p>todo</p>
+        </div>
+        <div className="flex-shrink-0 space-x-2">
+          <button className="text-teal rounded  border-2  bg-white p-2 text-black hover:border-black">
+            edit
+          </button>
+          <button className="text-teal rounded  border-2  bg-white p-2 text-black hover:border-black">
+            delete
+          </button>
+        </div>
+      </li>
+    </ul>
+  );
+};
+
+export default ItemTodo;
