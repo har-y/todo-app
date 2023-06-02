99036a4ace01c35210c74bdb6dfd00571205f34e
har-y <jakub.harenda@gmail.com>
2023-06-02 17:47:35 +0200

    add prototype todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <h1>todo app</h1>
+    <div className="min-h-screen bg-slate-400">
+      <div className="my mx-auto flex max-w-md flex-col items-center space-y-5 py-20 md:max-w-xl">
+        <h1 className="text-2xl font-bold">TODO LIST</h1>
+      </div>
     </div>
   );
 }
