5abf759f68ceaad87f0e2fef7c58ec9e13fef161
har-y <jakub.harenda@gmail.com>
2023-06-02 17:49:41 +0200

    add prototype 'add' todo in console.log

--- src/components/newtodo/NewTodo.jsx
+++ src/components/newtodo/NewTodo.jsx
@@ -1,11 +1,29 @@
+import { useState } from 'react';
+
 const NewTodo = () => {
+  const [newTodo, setNewTodo] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    console.log(newTodo);
+
+    setNewTodo('');
+  };
+
+  const inputChangeHandler = (e) => {
+    setNewTodo(e.target.value);
+  };
+
   return (
-    <form className="flex w-full ">
+    <form onSubmit={submitHandler} className="flex w-full ">
       <input
         className="mr-4 w-full appearance-none rounded border-2 px-3 py-2 text-black shadow outline-none hover:border-black"
         type="text"
         placeholder="enter new todo"
+        value={newTodo}
         required
+        onChange={inputChangeHandler}
       />
       <button
         type="submit"
