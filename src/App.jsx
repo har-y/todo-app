68b88d938585992f04761a2fb6fd870d024df3c3
har-y <jakub.harenda@gmail.com>
2023-06-03 15:12:45 +0200

    fix some code

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,11 @@ import todoData from './assets/data/todo-data';
 function App() {
   const [userData, setUserData] = useState(todoData);
 
-  const addTodoHandler = (newTodo) => {
-    setUserData((prev) => {
-      return [...prev, newTodo];
-    });
-  };
-
   return (
     <div className="min-h-screen bg-slate-400">
       <div className="my mx-auto flex max-w-md flex-col space-y-5 py-20 md:max-w-xl">
         <h1 className="mx-auto text-2xl font-bold">TODO LIST</h1>
-        <NewTodo onAddTodo={addTodoHandler} />
+        <NewTodo />
         <ListTodo data={userData} />
       </div>
     </div>
