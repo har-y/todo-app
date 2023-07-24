377dda049e81cad6af17085a67be231f21dbad67
har-y <jakub.harenda@gmail.com>
2023-07-25 00:33:38 +0200

    fix todo file data base

--- src/assets/data/todo-data.js
+++ src/assets/data/todo-data.js
@@ -1,6 +1,19 @@
 const todoData = [
-  { id: 1, text: 'lorem1 lorem1' },
-  { id: 2, text: 'lorem2 lorem2' },
+  {
+    id: 'b4e73bf2-97e4-4ae1-a3c7-bc192304e034',
+    text: 'lorem1 lorem1',
+    completed: false,
+  },
+  {
+    id: 'f9d4d650-d32e-4a06-9d87-760155e2bdf5',
+    text: 'lorem2 lorem2',
+    completed: true,
+  },
+  {
+    id: '0da7c889-c9ea-42fe-a9b5-50b09fe4e127',
+    text: 'lorem2 lorem2',
+    completed: false,
+  },
 ];
 
 export default todoData;
