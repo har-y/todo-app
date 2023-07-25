b176adcfd1a2b269cac2323fbefa467a165f7310
har-y <jakub.harenda@gmail.com>
2023-07-26 00:20:46 +0200

    add prototype 'sortBy' todo

--- src/components/listtodo/ListTodo.jsx
+++ src/components/listtodo/ListTodo.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ItemTodo from '../itemtodo/ItemTodo';
 
 const ListTodo = (props) => {
+  const [sortedBy, setSortedBy] = useState('input');
+
+  let sortedData;
+  if (sortedBy === 'input') {
+    sortedData = props.data;
+  }
+  if (sortedBy === 'text') {
+    sortedData = props.data.slice().sort((a, b) => {
+      return a.text.localeCompare(b.text);
+    });
+  }
+
+  const sortedByHandler = (e) => {
+    setSortedBy(e.target.value);
+  };
+
   return (
     <React.Fragment>
       {!props.data.length && (
@@ -11,7 +27,7 @@ const ListTodo = (props) => {
         </p>
       )}
       <ul className="w-full space-y-2 pt-5">
-        {props.data.map((arg) => {
+        {sortedData.map((arg) => {
           return (
             <ItemTodo
               key={arg.id}
@@ -26,7 +42,11 @@ const ListTodo = (props) => {
       </ul>
       {!!props.data.length && (
         <div className="w-16 border-b-2 border-neutral-100 border-opacity-50 py-2">
-          <select className="w-16 rounded-lg">
+          <select
+            className="w-16 rounded-lg"
+            value={sortedBy}
+            onChange={sortedByHandler}
+          >
             <option value="text">text</option>
             <option value="input">input</option>
           </select>
